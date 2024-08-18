a5f20c9a7d3e29f4ac7abfc5be9093c02bb65e0a
cyberneel - Neelesh <neelesh.chevuri25@gmail.com>
2024-08-17 22:11:35 -0500

    Making a faster cloud NLP

--- Main-Program/jsonify.py
+++ Main-Program/jsonify.py
@@ -1,6 +1,14 @@
 # import dependencies
 import torch
+import requests
 from transformers import pipeline
+
+###############################################\
+# NOTE: These arwe just my attempts using free
+# options I found on the internet. You can
+# most likely host your own local API for a
+# secure option if you need the privscy.
+###############################################
 
 # this is the model we will use, better ones prob exist but need finding
 generate_text = pipeline(model="databricks/dolly-v2-3b", torch_dtype=torch.bfloat16, trust_remote_code=True, device_map="auto")
@@ -15,3 +23,10 @@ def GetSpecsJson(raw=None):
     print(res[0]["generated_text"]) 
 
     return res[0]["generated_text"]
+
+# using cloudflare API for speed and for better models (Free at the time of coding)
+def GetSpecJsonCloud(raw=None):
+    # return if blank
+    if raw == None:
+        return
+    
